843cbb2fe61a09a5c72c11f3de1936a34a7f3943
diomedesfrias <121972066+diomedesfrias@users.noreply.github.com>
2023-01-26 03:13:05 -0400

    up

--- Tailspin.SpaceGame.Web.UITests/SeleniumTest.cs
+++ Tailspin.SpaceGame.Web.UITests/SeleniumTest.cs
@@ -31,7 +31,7 @@ namespace TechFabricSln.Test
             driver = new ChromeDriver();
             //driver.Navigate().GoToUrl("https://localhost:49385/");
             driver.Navigate().GoToUrl(Environment.GetEnvironmentVariable("SITE_URL"));
-            Thread.Sleep(10000);
+            Thread.Sleep(40000);
             // Locate the link by its ID and then click the link.
             ClickElement(FindElement(By.Id("loginBtn")));
 
