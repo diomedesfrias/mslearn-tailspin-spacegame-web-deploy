c1c4fba0970ab27f3b11274f72949aa1c5556455
diomedesfrias <121972066+diomedesfrias@users.noreply.github.com>
2023-01-31 11:30:20 -0400

    Added autocomplete

--- Tailspin.SpaceGame.Web/wwwroot/js/site.js
+++ Tailspin.SpaceGame.Web/wwwroot/js/site.js
@@ -2,51 +2,75 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
 $(function () {
     //overriding jquery-ui.autocomplete .js functions
     $.ui.autocomplete.prototype._renderMenu = function (ul, items) {
-        console.log("run");
-        console.log("ta");
+
         var self = this;
         //table definitions
-        ul.append("<table><thead><tr><th>ID#</th><th>Name</th><th>Cool&nbsp;Points</th></tr></thead><tbody></tbody></table>");
+        var tableDiv = $("#tableDiv");
+        tableDiv.empty();
+        tableDiv.append("<table class=" + '"' + 'table' + '"' + "><thead><tr><th>ID#</th><th>Name</th><th>category</th><th>city</th></tr></thead><tbody></tbody></table>");
+        
         $.each(items, function (index, item) {
-            self._renderItemData(ul, ul.find("table tbody"), item);
+            self._renderItemData(ul, tableDiv.find("table tbody"), item);
         });
     };
     $.ui.autocomplete.prototype._renderItemData = function (ul, table, item) {
+         
         return this._renderItem(table, item).data("ui-autocomplete-item", item);
     };
     $.ui.autocomplete.prototype._renderItem = function (table, item) {
         return $("<tr class='ui-menu-item' role='presentation'></tr>")
             //.data( "item.autocomplete", item )
-            .append("<td >" + item.id + "</td>" + "<td>" + item.value + "</td>" + "<td>" + item.cp + "</td>")
+            .append("<td >" + item.id + "</td>" + "<td>" + item.value + "</td>" + "<td>" + item.category + "</td>" + "<td>" + item.city + "</td>")
             .appendTo(table);
     };
-    //random json values
-    var projects = [
-        { id: 1, value: "Thomas", cp: 134 },
-        { id: 65, value: "Richard", cp: 1743 },
-        { id: 235, value: "Harold", cp: 7342 },
-        { id: 78, value: "Santa Maria", cp: 787 },
-        { id: 75, value: "Gunner", cp: 788 },
-        { id: 124, value: "Shad", cp: 124 },
-        { id: 1233, value: "Aziz", cp: 3544 },
-        { id: 244, value: "Buet", cp: 7847 }
-    ];
-    $("#project").autocomplete({
-        minLength: 1,
-        source: projects,
 
-        focus: function (event, ui) {
-            console.log(ui.item.value);
-            $("#project").val(ui.item.value);
-            $("#project-id").val(ui.item.id);
-            $("#project-description").html(ui.item.cp);
-            return false;
-        }//you can write for select too
-        /*select:*/
-    })
+    $.getJSON("js/profiles.json", function (json) {
+
+
+        var projects = [
+            { id: 1, value: "Thomas", category: "Afiliado", city: "San Juan" },
+            { id: 65, value: "Richard", category: "Suplidor", city: "Agudillas" },
+            { id: 235, value: "Harold", category: "Afiliado", city: "San Juan" },
+            { id: 78, value: "Santa Maria", category: "Suplidor", city: "San Juan" },
+            { id: 75, value: "Gunner", category: "Afiliado", city: "San Juan" },
+            { id: 124, value: "Shad", category: "Suplidor", city: "San Juan" },
+            { id: 1233, value: "Aziz", category: "Medico", city: "Toa Alta" },
+
+            { id: 244, value: "Olvin", category: "Suplidor", city: "San Juan" },
+            { id: 1, value: "Carlos", category: "Afiliado", city: "Toa Alta" },
+            { id: 65, value: "Juan Jose", category: "Afiliado", city: "Agudillas" },
+            { id: 235, value: "Harold", category: "Afiliado", city: "Toa Alta" },
+            { id: 78, value: "Santa Maria", category: "Medico", city: "San Juan" },
+            { id: 75, value: "Rafael", category: "Afiliado", city: "Agudillas" },
+            { id: 124, value: "Yancarlos", category: "Medico", city: "Agudillas" },
+            { id: 1233, value: "Maria", category: "Medico", city: "Toa Alta" },
+            { id: 244, value: "Jonas", category: "Medico", city: "Toa Alta" }
+        ];
+        console.log(json);
+        console.log(projects);
+
+        $("#project").autocomplete({
+            minLength: 1,
+            source: projects,
+
+            focus: function (event, ui) {
+                console.log(ui.item.id);
+                $("#project").val(ui.item.value);
+                $("#project-id").val(ui.item.id);
+                // $("#project-description").html(ui.item.category);
+                return false;
+            }//you can write for select too
+            /*select:*/
+        })
+
+    });
+
+
 });
 
 $(document).ready(function () {
@@ -88,7 +112,7 @@ function loginUserFlow() {
         $("#signUpBtn").hide();
         $("#cancel-login-btn").hide();
         $("#dontHaveAccountLabel").hide();
-        $("#userName").prop("disabled", true);
+        $("#value").prop("disabled", true);
     });
 
     $("#okButton").click(function () {
